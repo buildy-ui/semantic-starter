b0219c976c806af6c5de753862a051fbca3a7a19
alexy-os <agorbunoff.ru@gmail.com>
2025-06-19 14:49:17 +0300

    Created a script for generating a pure static HTML website. See the example in the 'www' folder

--- src/app/layouts/MainLayout.tsx
+++ src/app/layouts/MainLayout.tsx
@@ -1,5 +1,5 @@
 import { ReactNode } from "react";
-import { Helmet } from 'react-helmet-async';
+//import { Helmet } from 'react-helmet-async';
 import { Navigation } from "@/app/components/Navigation";
 import { Sidebar } from "@/app/components/Sidebar";
 import { SiteLogo } from "@/app/components/SiteLogo";
@@ -22,12 +22,12 @@ interface RootLayoutProps {
 export const MainLayout = ({ title, description, children }: RootLayoutProps) => {
   return (
     <>
-      <Helmet>
+      {/*<Helmet>
         <title>{title}</title>
         <meta name="description" content={description} />
         <meta property="og:title" content={title} />
         <meta property="og:description" content={description} />
-      </Helmet>
+      </Helmet>*/}
       
       <SheetLayout>
         <Navigation />
